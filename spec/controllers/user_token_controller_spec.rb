74722d9f3fa73d6bd5dfc27bf43428b570a7ebb8
Arturo Guzman <arturo@kisurfer.com>
2017-02-28 21:02:11 -0500

    chore(specs): fix rspec warnings

--- spec/controllers/user_token_controller_spec.rb
+++ spec/controllers/user_token_controller_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe UserTokenController, type: :controller do
   describe 'POST index' do
     let(:user) { FactoryGirl.create(:user, email: 'john@example.com', password: 'helloworld') }
 
-    it 'returns a JWT token when submitting valid credentials' do
-      post :create, auth: { email: user.email, password: 'helloworld' }
+    it 'responds a JWT token when submitting valid credentials' do
+      post :create, params: { auth: { email: user.email, password: 'helloworld' } }
 
       expect(response).to have_http_status(:created)
       expect(response.body).to include('jwt')
     end
 
     it 'fails when submitting invalid credentials' do
-      post :create, auth: { email: user.email, password: 'thisisbad' }
+      post :create, params: { auth: { email: user.email, password: 'thisisbad' } }
       expect(response).to have_http_status(:not_found)
     end
   end
